caeb3844979cd535619a0445801fc91ee91456da
alunoEtec <aula@etec.com>
2025-10-23 22:40:51 -0300

    sim

--- lista2/exercicio5.py
+++ lista2/exercicio5.py
@@ -3,3 +3,4 @@ num2 = float(input("Digite o segundo numero: "))
 num3 = float(input("Digite o terceiro numero: "))
 num4 = float(input("Digite o quarto numero: "))
 
+
---
+++ lista2/exercicio7.py
@@ -0,0 +1,2 @@
+vel_maxima = int(input("Qual a velocidade maxima permitida: "))
+vel_motorista = int(input("Qual a valocidade que o motorista passou: "))
